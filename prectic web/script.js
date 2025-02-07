03770b239d658b5a3b4329a810fe38bda36b6e0c
sureshjoshi88 <sj3795789@gmail.com>
2025-02-07 15:00:04 +0530

    add new java script file

---
+++ prectic web/script.js
@@ -0,0 +1,28 @@
+let main = document.getElementById("number")
+let main1 = document.getElementById("number1")
+let main2 = document.getElementById("number2")
+
+function getvalue(){
+  for(let i = 1;i<=1994;i++){
+    setTimeout(()=>{
+      main.innerHTML = i;
+      main.style.fontSize = "45px"
+
+    },i*3);
+  }
+  for(let i = 1;i<=2000;i++){
+    setTimeout(()=>{
+      main1.innerHTML = i;
+      main1.style.fontSize = "45px"
+
+    },i*3);
+  }
+  for(let i = 1;i<=2654;i++){
+    setTimeout(()=>{
+      main2.innerHTML = i;
+      main2.style.fontSize = "45px"
+
+    },i*3);
+  }
+}
+getvalue();
